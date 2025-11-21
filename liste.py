a5554c0a52abc3828067b866e0253e3b0758bc55
Doremla <mattiadorella70@gmail.com>
2025-11-21 16:07:32 +0100

    Aggiunta stampa somma e media

--- liste.py
+++ liste.py
@@ -1,6 +1,8 @@
 Ngrades = int(input("Dimmi il  numero dei voti: "));
+
 while Ngrades <= 0:
     Ngrades = int(input("Valore negativo non valido, riprova: "));
+    
 print(f"Il numero dei voti inserito è: {Ngrades}"); 
 grades = [];
 count = 0;
@@ -16,3 +18,15 @@ i = 0;
 while i < count:
      print(f"voto: {grades[i]}");
      i += 1;
+
+i2 = 0;
+summ = 0;
+
+while i2 < count:
+     summ += grades[i2];
+     print(grades[i2]);
+     i2 += 1;
+     
+print(f"la somma è: {summ}");
+
+print("la media è: ", summ/count);
