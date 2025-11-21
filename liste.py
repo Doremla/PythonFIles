5e70ae7f1aafce75ecbab3b4093bc38fae01a1e0
Doremla <mattiadorella70@gmail.com>
2025-11-21 15:24:58 +0100

    Migliorato il file per far si che la lista riceve dei valori in input e cambia dinamicamente dando un feedback dell artista inserito

--- liste.py
+++ liste.py
@@ -1,6 +1,15 @@
-fav_artisti = ["Jimi Hendrix", "Dua Lipa", "Eminem", "Arctic Monkeys"];
+artista = "";
+fav_artisti = [];
+
+while artista != "q":
+    artista = input("Dammi il nome dell'artista(premi 'q' per chiudere): ");
+    print(f"artista inserito è: {artista}");
+    #fav_artisti = ["Jimi Hendrix", "Dua Lipa", "Eminem", "Arctic Monkeys"];
+    fav_artisti.append(artista);
+    print(len(fav_artisti));
 
 i = 0;
-while i <= len(fav_artisti) - 1:
+
+while i < len(fav_artisti):
      print(fav_artisti[i]);
      i += 1;
