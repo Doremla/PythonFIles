89e884d442a7fba655f4503c5dcedd36c53112d8
Doremla <mattiadorella70@gmail.com>
2025-11-21 15:36:08 +0100

    Cambiato il file, ora chiede il numero dei voti per poi chiedere i voti e stamparli

--- liste.py
+++ liste.py
@@ -1,15 +1,16 @@
-artista = "";
-fav_artisti = [];
+Ngrades = int(input("Dimmi il  numero dei voti: "));
+print(f"Il numero dei voti inserito è: {Ngrades}"); 
+grades = [];
+count = 0;
 
-while artista != "q":
-    artista = input("Dammi il nome dell'artista(premi 'q' per chiudere): ");
-    print(f"artista inserito è: {artista}");
-    #fav_artisti = ["Jimi Hendrix", "Dua Lipa", "Eminem", "Arctic Monkeys"];
-    fav_artisti.append(artista);
-    print(len(fav_artisti));
+while count < Ngrades:
+    count += 1;
+    grade = int(input("Dammi il voto: "));
+    print(f"Il voto inserito è: {grade}");
+    grades.append(grade);
 
 i = 0;
 
-while i < len(fav_artisti):
-     print(fav_artisti[i]);
+while i < count:
+     print(f"voto: {grades[i]}");
      i += 1;
